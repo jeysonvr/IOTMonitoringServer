8365eb751d60df352aea5b1ed112a267d4f8d440
Jeyson Vega <jvega@univision.net>
2023-09-12 12:41:45 -0500

    update vars

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.237.246.30"]
+ALLOWED_HOSTS = ["localhost", "54.162.21.115"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.235.58.103",  # Dirección IP de la base de datos
+        "HOST": "54.162.103.152",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.225.4.10"
+MQTT_HOST = "34.235.119.136"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
