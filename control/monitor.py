b2bf243a84363d3004a42e8b813f64a8a30b9a00
Jeyson Vega <jvega@univision.net>
2023-09-17 10:40:45 -0500

    test control

--- control/monitor.py
+++ control/monitor.py
@@ -59,7 +59,8 @@ def analyze_data():
         # Si la ccantidad de mediciones realizadas en la última hora es menor a 100
         # Se envía una alerta para informar que la cantidad de datos aún no es suficiente
         # para ser completamente fiable
-        if amount_value < 100:
+        #if item["amount_value"] < 100:
+        if 10 < 100:
             message = "ALERT AMOUNT {} {} {}".format(variable, min_value, max_value)
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
             print(datetime.now(), "Sending amount alert to {} {}".format(topic, variable))
